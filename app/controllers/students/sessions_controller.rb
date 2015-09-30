d2dd6d89647231e8dbbd3bdeab05567142296362
naomooo <naomooo.official@gmail.com>
2015-09-30 20:51:44 +0900

    ログイン、ログアウト機能について

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -12,9 +12,9 @@ class Students::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    
+  end
 
   # protected
 
