5493a4d16d252dda57f28b78f76ade9952e2b719
naomooo <naomooo.official@gmail.com>
2015-09-30 23:01:52 +0900

    ログイン画面

--- app/controllers/workers/sessions_controller.rb
+++ app/controllers/workers/sessions_controller.rb
@@ -12,9 +12,9 @@ class Workers::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    
+  end
 
   # protected
 
